776eae018ea5944d70597ab789dd9b1c59f61523
Pedro Barros <pedrobars7a@gmail.com>
2023-01-12 19:50:10 -0300

    Instalando mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
-
-const app = express();
+import mongoose from 'mongoose';
 
 const port = 3001;
-app.listen(3001, () => {
-  console.log(`🔥 Server is running on http://localhost:${port}`);
-});
+
+mongoose
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
+
+    app.listen(3001, () => {
+      console.log(`🔥 Server is running on http://localhost:${port}`);
+    });
+  })
+  .catch(() => console.log('Erro ao conectar ao mongodb'));
