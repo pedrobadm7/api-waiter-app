665788f59e816cec02d6cd3d330e6016ec420e01
Pedro Barros <pedrobars7a@gmail.com>
2023-01-12 19:20:33 -0300

    setup server

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 
 const app = express();
 
+const port = 3001;
 app.listen(3001, () => {
-  console.log('🔥 Server is running on http://localhost:3001');
+  console.log(`🔥 Server is running on http://localhost:${port}`);
 });
