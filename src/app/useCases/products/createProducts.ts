7cadedb67ca410117d0cf45b0994073c90ec47db
Pedro Barros <pedrobars7a@gmail.com>
2023-01-21 10:20:35 -0300

    Create createProduct

---
+++ src/app/useCases/products/createProducts.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express';
+import { Product } from '../../models/Product';
+
+export async function createProduct(req: Request, res: Response) {
+  try {
+    null;
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+}
