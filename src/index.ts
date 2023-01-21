1cca9b19a6c89444ef4104b0649217ccc3946dc8
Pedro Barros <pedrobars7a@gmail.com>
2023-01-21 11:05:27 -0300

    Criação de static path

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import express, { json } from 'express';
 import mongoose from 'mongoose';
 
@@ -9,6 +10,11 @@ mongoose
   .connect('mongodb://localhost:27017')
   .then(() => {
     const app = express();
+
+    app.use(
+      '/uploads',
+      express.static(path.resolve(__dirname, '..', 'uploads'))
+    );
     app.use(express.json());
     app.use(router);
 
