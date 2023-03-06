d415e504e5888aa6d5f775c177ffb5cfa9944cb7
Pedro Barros <pedrobars7a@gmail.com>
2023-03-06 19:21:27 -0300

    feature: Create method list and findByName

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,20 +1,29 @@
-import { CategoryModel } from '../models/Category';
-
-interface ICreateCategoryDTO {
-  name: string;
-  icon: string;
-}
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { CategoryModel, ICategoryDTO } from '../models/Category';
 
 interface ICategoriesRepository {
-  create({ name, icon }: ICreateCategoryDTO): Promise<void>;
+  create({ name, icon }: ICategoryDTO): Promise<void>;
+  list(): Promise<ICategoryDTO[]>;
 }
 
 export class CategoriesRepository implements ICategoriesRepository {
-  async create({ name, icon }: ICreateCategoryDTO): Promise<void> {
+  async create({ name, icon }: ICategoryDTO): Promise<void> {
     const category = await CategoryModel.create({
       icon,
       name,
     });
     await category.save();
+  }
+
+  async list(): Promise<ICategoryDTO[]> {
+    const categories = await CategoryModel.find();
+    return categories;
+  }
+
+  async findByName(name: string): Promise<ICategoryDTO[]> {
+    const categories = await CategoryModel.find({
+      name: { $regex: name, $options: 'i' },
+    });
+    return categories;
   }
 }
