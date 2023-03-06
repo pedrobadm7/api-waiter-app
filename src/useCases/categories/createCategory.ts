0fa6fe0bf35ab22b507a5bb46ae7866a617602c6
Pedro Barros <pedrobars7a@gmail.com>
2023-03-06 19:21:44 -0300

    feature: Create categoryAlreadyExists

--- src/useCases/categories/createCategory.ts
+++ src/useCases/categories/createCategory.ts
@@ -7,6 +7,12 @@ export async function createCategory(req: Request, res: Response) {
   try {
     const { icon, name } = req.body;
 
+    const categoryAlreadyExists = await categoriesRepository.findByName(name);
+
+    if (categoryAlreadyExists) {
+      return res.status(400).json({ error: 'Category Already Exists!' });
+    }
+
     if (!name) {
       return res.send(400).json({
         error: 'Name is required',
